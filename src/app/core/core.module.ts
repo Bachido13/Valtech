b5621e52a7ae170b1d7aa08a5a32998520a9b6d8
Bachido13 <jonathanrambach@hotmail.fr>
2022-12-08 22:37:01 +0100

    setup core-module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,14 +3,32 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { HeaderComponent } from './components/header/header.component';
-
+import { registerLocaleData } from '@angular/common';
+import localeFr from '@angular/common/locales/fr';
+import * as fr from '@angular/common/locales/fr';
 
 @NgModule({
+
   declarations: [
     HeaderComponent
   ],
+
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
+    HttpClientModule
+  ],
+
+  exports: [
+    HeaderComponent
+  ],
+
+  providers: [
+    { provide: LOCALE_ID, useValue: 'fr-FR'}
   ]
 })
-export class CoreModule { }
+export class CoreModule { 
+  constructor() {
+    registerLocaleData(fr.default);
+  }
+}
