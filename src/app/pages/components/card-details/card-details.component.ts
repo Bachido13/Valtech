bc65e11b8bafd1bf3d783a773cd343a838348dbb
Bachido13 <jonathanrambach@hotmail.fr>
2022-12-08 23:08:25 +0100

    card-details component

---
+++ src/app/pages/components/card-details/card-details.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { Card } from 'src/app/core/models/card.model';
+import { CardsServices } from 'src/app/core/services/card.services';
+import { ActivatedRoute, Router } from '@angular/router';
+import { catchError, EMPTY, map, Observable, switchMap, take, tap } from 'rxjs';
+
+
+@Component({
+  selector: 'app-card-details',
+  templateUrl: './card-details.component.html',
+  styleUrls: ['./card-details.component.scss']
+})
+export class CardDetailsComponent implements OnInit {
+
+  card!: Card;
+  card$!: Observable<Card>;
+  errorMsg!: string;
+
+  constructor(private cardsServices: CardsServices,
+              private route: ActivatedRoute,
+              private router: Router) { }
+
+  ngOnInit(): void {
+    this.card$ = this.route.params.pipe(
+      map(params => params['id']),
+      switchMap(id => this.cardsServices.getCardById(id)),
+      tap(card => {
+        console.log(card);
+        this.card = card;
+      })
+    )
+  }
+
+  onBack() {
+    this.router.navigate(['card/home']);
+  }
+
+}
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardComponent } from './components/card/card.component';
 import { HomeComponent } from './components/home/home.component';
+import { CardDetailsComponent } from './components/card-details/card-details.component';
 
 
 
 @NgModule({
   declarations: [
     CardComponent,
-    HomeComponent
+    HomeComponent,
+    CardDetailsComponent
   ],
   imports: [
     CommonModule
